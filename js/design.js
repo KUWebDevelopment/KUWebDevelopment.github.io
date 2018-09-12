8de59c25d34f5f0aa5ed6b47720c8f7bcad6da0e
Giovaartavia <giovaartavia@hotmail.com>
2018-09-12 00:26:03 -0500

    Centered Navbar and fixed automatic date
    Centered Navbar and fixed automatic date

--- js/design.js
+++ js/design.js
@@ -28,13 +28,14 @@ $(document).ready(function(){
     });
 
     //Automatically change next meeting date
-    // let nextMeeting = new Date(); //Starts as current date
-    // let prevMeeting = $("#nextMeetingDate");
+    let nextMeeting = new Date(); //Starts as current date
+    let prevMeeting = $("#nextMeetingDate");
 
-    // if(nextMeeting.getDay() == 3){ //if date is Wednesday, change to next Tuesday
-    //   nextMeeting.setDate(nextMeeting.getDate() + 6);
-    // }
-    // prevMeeting.html((nextMeeting.getMonth() + 1) + '/' + nextMeeting.getDate() + '/' + nextMeeting.getFullYear());
+    while(nextMeeting.getDay() != 2)
+    {
+      nextMeeting.setDate(nextMeeting.getDate() + 1);
+    }
+    prevMeeting.html((nextMeeting.getMonth() + 1) + '/' + nextMeeting.getDate() + '/' + nextMeeting.getFullYear());
     
     //Adjust Navbar
     var top = $('.infoBox1').offset().top;
